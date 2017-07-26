cb8cdb44bcc68376212f879e79c6da526f432470
zw2582 <2582308253@qq.com>
2017-07-26 23:52:52 +0800

    nio的读、写、创建文件的channel方式

--- src/javabase/lean/io/ReadWriteAndCreate.java
+++ src/javabase/lean/io/ReadWriteAndCreate.java
@@ -3,16 +3,31 @@ package javabase.lean.io;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FilePermission;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.FileAttributeView;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 读、写、创建文件
@@ -121,10 +136,85 @@ public class ReadWriteAndCreate {
 		}
 	}
 	
+	/**
+	 * 使用channel读取数据
+	 * @param file
+	 * @throws IOException
+	 */
+	public static void ChannelRead(String file) throws IOException {
+		//定义路径
+		Path path = Paths.get(file);
+		
+		//开启通道，默认是reading
+		try(SeekableByteChannel bchannel = Files.newByteChannel(path)) {
+			
+			//定义byteBuffer
+			ByteBuffer allocate = ByteBuffer.allocate(100);
+			//读取数据
+			while(bchannel.read(allocate) > 0) {
+				allocate.rewind();
+				System.out.println(Charset.defaultCharset().decode(allocate));
+				allocate.flip();
+			}
+		}
+	}
+	
+	public static void channelWrite(String file) throws IOException {
+		//制作文件路径
+		Path path = Paths.get(file);
+		
+		//编辑文件操作模式
+		Set<OpenOption> options = new HashSet<>();
+		options.add(StandardOpenOption.APPEND);
+		options.add(StandardOpenOption.CREATE);
+		
+		//编辑文件权限
+		Set<PosixFilePermission> fs = PosixFilePermissions.fromString("rw-r-----");
+		FileAttribute<Set<PosixFilePermission>> attrs = PosixFilePermissions.asFileAttribute(fs);
+
+		//编辑写入数据
+		String data = "我是中国人，我爱自己的祖国";
+		ByteBuffer byteBuffer = Charset.defaultCharset().encode(data);
+		
+		//开启通道，写入数据
+		try(SeekableByteChannel sbc = Files.newByteChannel(path, options, attrs)) {
+			sbc.write(byteBuffer);
+		}
+	}
+	
+	/**
+	 * 创建文件，如果文件已存在，抛出错误
+	 * 可以使用newOutputStream创建文件
+	 * @param file
+	 * @throws IOException 
+	 */
+	public static void createFile(String file) throws IOException {
+		Path createFile = Files.createFile(Paths.get(file));
+	}
+	
+	/**
+	 * 创建临时文件
+	 * @throws IOException
+	 */
+	public static void createTmpFile() throws IOException {
+		Path ctf = Files.createTempFile("d", ".myapp");
+		
+		System.out.println(ctf.toAbsolutePath());
+		
+		Files.createTempFile(Paths.get("."), "d", ".myapp");
+	}
+	
 	public static void main(String[] args) throws IOException {
 //		commonForSmall("1_c.txt");
 //		bufferedRead("1_w.txt");
 //		bufferedWrite("1_w.txt");
-		outputStreamWriter("1_w.txt");
+//		outputStreamWriter("1_w.txt");
+//		ChannelRead("1_channel.txt");
+		
+//		channelWrite("1_channel.txt");
+		File file = new File("cac.txt");
+//		createFile("create.txt");
+		createTmpFile();
+		
 	}
 }
