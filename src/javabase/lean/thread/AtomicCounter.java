09f8fea891ee5ce16e2a7772ea8d1167a9812b60
zhouw <2582308253@qq.com>
2017-08-12 13:53:29 +0800

    原子变量

---
+++ src/javabase/lean/thread/AtomicCounter.java
@@ -0,0 +1,21 @@
+package javabase.lean.thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 利用原子变量避免线程干扰
+ * @author wei.w.zhou.integle.com
+ * @copyright 2017年8月12日下午1:48:52
+ */
+public class AtomicCounter {
+
+	private AtomicInteger i = new AtomicInteger(0);
+	
+	public void add() {
+		i.incrementAndGet();
+	}
+	
+	public void dec() {
+		i.decrementAndGet();
+	}
+}
