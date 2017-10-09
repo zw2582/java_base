006d75ad47945f853c714f98f46900666cf20d04
zw2582 <2582308253@qq.com>
2017-10-10 06:08:03 +0800

    fork/join框架

---
+++ src/javabase/lean/thread/ForkBlur.java
@@ -0,0 +1,17 @@
+package javabase.lean.thread;
+
+import java.util.concurrent.RecursiveAction;
+/**
+ * fork/join框架
+ * 用于将任务分解成更小的任务，从而利用多个处理器增强处理能力，以达到提高应用性能
+ * @author zhangjiao
+ *
+ */
+public class ForkBlur extends RecursiveAction{
+
+	@Override
+	protected void compute() {
+		
+	}
+
+}
